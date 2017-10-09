9b6ae8fb5ff7f1ab34bcc8fa9c1ef21d8de88448
MyGitHubFrom16 <kinash-2012@mail.ru>
2017-10-09 08:24:43 +0300

    local

--- src/let.js
+++ src/let.js
@@ -17,10 +17,46 @@ if(true){
 var buttons = document.querySelectorAll('button');
 for(let i = 0; i < buttons.length; i++){
     var button = buttons[i];
-    button.innerText = button.innerText + i; 
-    button.onclick = function(event){
-        console.log(i);
-    }
+    button.innerText = button.innerText + i;
+
+        function buttonMouseOver(event) {
+            this.classList.add('over');
+            this.classList.add('start-animation');
+            console.log('buttonMouseOver:: adding:: .over & .start-animation');
+        }
+
+            function buttonTransitionend(event) {
+                console.log(event.propertyName);
+                console.log(event.elapsedTime);
+                if (this.classList.contains('start-animation') && this.classList.contains('over')) {
+                    // завершена анимация при наведении мыши на едемент
+                    this.classList.remove('start-animation');
+                    this.classList.add('animating');
+                    console.log('IS buttonTransitionend! :: remove:: .start-animation & adding:: .animating');
+                } else if (this.classList.contains('finish-animation') && !this.classList.contains('over')){
+                    // завершена анимация при выхода мыши с елемента
+                    this.classList.remove('finish-animation');
+                    console.log('IS buttonTransitionend! :: remove:: .animating');
+                }
+            }
+
+        function buttonMouseLeave(event) {
+            this.classList.remove('over');
+            this.classList.remove('animating');
+            this.classList.add('finish-animation');
+            console.log('buttonMouseLeave:: remove:: .over, .animating & adding:: .finish-animation');
+        }
+
+    function buttonMouseMove(event) {
+        console.log('mousemove над "button#" ', i);
+    };
+
+    // button. removeEventListener('mousemove');
+
+    button.addEventListener('mouseover', buttonMouseOver);
+    button.addEventListener('transitionend', buttonTransitionend);
+    button.addEventListener('mousemove', buttonMouseMove);
+    button.addEventListener('mouseleave', buttonMouseLeave);
 }
 
 console.log(buttons);
